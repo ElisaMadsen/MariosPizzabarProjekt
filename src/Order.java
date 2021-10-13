8943ea83773cb96f6fe838ba34bff3ad4f19804f
Meerwaldten <Mikkel_m_j@hotmail.com>
2021-10-13 11:19:17 +0200

    Commit fra skolen 1.1

--- src/Order.java
+++ src/Order.java
@@ -22,41 +22,6 @@ public class Order {
         return "Bestilling til " + "navn: " + customerName + " som har bestilt: "+numberOfPizzas+" pizzaer, med afhentningstidspunkt: " + pickUpTime;
     }
 
-    public static void createNewOrdre() {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Skriv dit navn:");
-        String ordreName = scanner.nextLine();
-        System.out.println("Antal pizzaer:");
-        int antalPizzaer = scanner.nextInt();
-        System.out.println("Nummer på kundens ønskede pizza:");
-
-        Pizza margherita = new Pizza(1, "Margherita", 50);
-        Pizza pepperoni = new Pizza(2, "Pepperoni", 60);
-        Pizza capricciossa = new Pizza(3, "Capricciossa", 72);
-        Pizza soloMio = new Pizza(4, "Solo mio", 75);
-        Pizza italiana = new Pizza(5, "Italiana", 80);
-
-        Pizza[] menukort = {margherita, pepperoni, capricciossa, soloMio, italiana};
-        ArrayList<Pizza> valgtePizzaer = new ArrayList();
-
-        for (int i = 0; i < menukort.length; i++) {
-            System.out.println(menukort[i]);
-        }
-        for (int i = 0; i < antalPizzaer; i++) {
-            Pizza pizzaChoice = menukort[scanner.nextInt() - 1];
-            valgtePizzaer.add(pizzaChoice);
-            System.out.println(pizzaChoice);
-        }
-        scanner.nextLine();
-        System.out.println("Skriv hvornår du kan hente din ordre:");
-        String pickUpOrdreTime = scanner.nextLine();
-        Order newOrder123 = new Order(ordreName, antalPizzaer, valgtePizzaer, pickUpOrdreTime);
-        System.out.println(newOrder123);
-        for (Pizza a : valgtePizzaer) {
-            System.out.println(a);
-        }
-        orderList.add(newOrder123);
-    }
 
     public static ArrayList<Order> getOrderList() {
         return orderList;
